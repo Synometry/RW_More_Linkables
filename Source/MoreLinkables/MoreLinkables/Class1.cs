b14ffb6af808d2e45e11d040685d8526c2904a53
Holden Bailey <hobbes3303@gmail.com>
2023-06-21 00:59:37 -0700

    robot assistant tweaks

--- Source/MoreLinkables/MoreLinkables/Class1.cs
+++ Source/MoreLinkables/MoreLinkables/Class1.cs
@@ -39,4 +39,65 @@ namespace Hobbes.MoreLinkables {
                 Graphic.Draw(GenThing.TrueCenter(parent.Position, parent.Rotation, parent.def.size, Y), parent.Rotation, parent);
         }
     }
+
+    public class CompProperties_WorkHelper : CompProperties {
+        public float radius = 5f;
+        public int tickInterval = 100;
+
+        public CompProperties_WorkHelper() {
+            compClass = typeof(CompWorkHelper);
+        }
+    }
+
+    public class CompWorkHelper : ThingComp {
+
+        protected CompPowerTrader compPower;
+
+        private CompProperties_WorkHelper Props => (CompProperties_WorkHelper)props;
+        
+        private IntVec3[] areaCache;
+
+        private IntVec3[] Area {
+            get {
+                if (areaCache == null) {
+                    areaCache = GenRadial.RadialCellsAround(parent.Position, Props.radius, false).ToArray();
+                }
+                return areaCache;
+            }
+        }
+
+        public override void CompTickLong() {
+            base.CompTickLong();
+            if (compPower.PowerOn) {
+                DoWork();
+            }
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad) {
+            base.PostSpawnSetup(respawningAfterLoad);
+            compPower = parent.GetComp<CompPowerTrader>();
+        }
+
+        private void DoWork() {
+            IEnumerable<Thing> things = WorkTargets().TakeRandom(1);
+            foreach (Thing t in things) {
+                UnfinishedThing ut = (UnfinishedThing) t;
+                ut.workLeft = Math.Max(1f, ut.workLeft - 60f);
+                //Log.Message($"work being done on {t.Label}; work left: {((UnfinishedThing) t).workLeft}");
+            }
+        }
+
+        private List<Thing> WorkTargets() {
+            List<Thing> targets = new List<Thing>();
+            for (int i = 0; i < Area.Length; i++) {
+                if (!Area[i].InBounds(parent.Map)) {
+                    continue;
+                }
+                foreach (Thing t in Area[i].GetItems(parent.Map)) {
+                    if (t.def.isUnfinishedThing && ((UnfinishedThing)t).workLeft > 1f) targets.Add(t);
+                }
+            }
+            return targets;
+        }
+    }
 }
